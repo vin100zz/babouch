9cd23937bd98bd2a1af5fd439d4fda5d957848c6
LAPTOP-SMMFSP13\carle <carle.rene@wanadoo.fr>
2023-01-05 12:05:06 +0100

    Recherche
    (cherry picked from commit 6cb8bcce8f83d4deb8dc28bc4ca2d80b9da48c52)

--- _moule/lib/pages/welcomeCtrl.js
+++ _moule/lib/pages/welcomeCtrl.js
@@ -1,8 +1,35 @@
 app.controller('WelcomeCtrl', ['$scope', '$timeout', 'ChapitresSvc', function($scope, $timeout, ChapitresSvc) {
 
+$scope.suggestions = [];
+
+$scope.searchInput = '';
+
 $timeout(function () {
   $scope.chapitres = ChapitresSvc.chapitres;
   $scope.titre_site = ChapitresSvc.titre_site();
  }, 1000);
+
+$scope.onSearchChange = function () {
+  $scope.suggestions = [];
+  if ($scope.searchInput.length > 1) {
+    var chapitres = $scope.chapitres();
+    addSuggestions(chapitres, '');
+    $scope.suggestions = $scope.suggestions.sort((s1, s2) => s1.label.localeCompare(s2.label));
+  }
+}
+
+addSuggestions = function (chapitres, path) {
+  (chapitres || []).forEach(chapitre => {
+    if (chapitre.titre && chapitre.titre.toLowerCase().match($scope.searchInput.toLowerCase())) {
+      $scope.suggestions.push({label: chapitre.titre, link: path + chapitre.dossier});
+    }
+    addSuggestions(chapitre.chapitres, path + chapitre.dossier + '/');
+  });
+}
+
+$scope.onClick = function () {
+  $scope.searchInput = '';
+  $scope.suggestions = [];
+}
    
 }]);
